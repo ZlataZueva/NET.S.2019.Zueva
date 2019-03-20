29876849b2625ff210f5030f0ee4d6359e15b926
Zlata Zueva <zlatka.zueva@gmail.com>
2019-03-20 23:29:24 +0300

    Day 1 sorting functions

---
+++ NET.S.2019.Zueva.01/NET.S.2019.Zueva.01Tests/ArrayExtensionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NET.S._2019.Zueva._01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Zueva._01.Tests
+{
+    [TestClass()]
+    public class ArrayExtensionTests
+    {
+        [TestMethod()]
+        public void QuickSortedTest()
+        {
+            //Arrange
+            int[] arrayToSort = new int[] { 1, 5, 6, 3, 8, 10};
+            int[] expectedResult = new int[] { 1, 3, 5, 6, 8, 10 };
+
+            //Act
+            arrayToSort.QuickSort();
+            int[] actualResult = arrayToSort;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult, "Regular array of 6 elements sorting with quick sort is not correct.");
+        }
+    }
+}
---
+++ NET.S.2019.Zueva.01/NET.S.2019.Zueva.01Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("NET.S.2019.Zueva.01Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NET.S.2019.Zueva.01Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Задание значения false для атрибута ComVisible приведет к тому, что типы из этой сборки станут невидимыми 
+// для COM-компонентов.  Если к одному из типов этой сборки необходимо обращаться из 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Если данный проект доступен для модели COM, следующий GUID используется в качестве идентификатора библиотеки типов
+[assembly: Guid("03d9a1ef-7ef4-4f45-b7ab-7cd8aadbdccb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
