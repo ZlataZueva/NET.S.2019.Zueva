ddc0d80eea3bec6c999498d379be9bf655f4173c
Zlata Zueva <zlatka.zueva@gmail.com>
2019-03-21 17:02:32 +0300

    added NUnit Testing

---
+++ ArraySortingTesting/NUnitTesting.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using NET.S._2019.Zueva._01;
+using System;
+
+namespace ArraySortingTesting
+{
+    [TestFixture]
+    public class NUnitTesting
+    {
+        [Test]
+        public void ValidArrayQuickSortTest ()
+        {
+            //Arrange
+            int[] arrayToSort = new int[] { 5, 3, 6, 12, 9, 1 };
+            int[] expectedResult = new int[] { 1, 3, 5, 6, 9, 12 };
+
+            //Act
+            int[] actualResult = arrayToSort.QuickSorted();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void EmptyArrayQuickSortTest ()
+        {
+            //Arrange
+            int[] arrayToSort = new int[] { };
+            int[] expectedResult = new int[] { };
+
+            //Act
+            int[] actualResult = arrayToSort.QuickSorted();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void NullArrayQuickSortTest ()
+        {
+            //Arrange
+            int[] arrayToSort = null;
+
+            //Assert
+            Assert.Throws<ArgumentNullException>( () => arrayToSort.QuickSorted() );
+        }
+
+
+        [Test]
+        public void ValidArrayMergeSortTest()
+        {
+            //Arrange
+            int[] arrayToSort = new int[] { 5, 3, 6, 12, 9, 1 };
+            int[] expectedResult = new int[] { 1, 3, 5, 6, 9, 12 };
+
+            //Act
+            int[] actualResult = arrayToSort.MergeSorted();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void EmptyArrayMergeSortTest()
+        {
+            //Arrange
+            int[] arrayToSort = new int[] { };
+            int[] expectedResult = new int[] { };
+
+            //Act
+            int[] actualResult = arrayToSort.MergeSorted();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void NullArrayMergeSortTest()
+        {
+            //Arrange
+            int[] arrayToSort = null;
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => arrayToSort.MergeSorted());
+        }
+    }
+}
---
+++ NET.S.2019.Zueva/ArraySortExtension.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Zueva._01
+{
+    public static class ArraySortExtension
+    {
+        public static int[] QuickSorted(this int[] array)
+        {
+            if (array != null)
+            {
+                return QuickSort(array, 0, array.Length - 1);
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        private static int[] QuickSort(int[] a, int i, int j)
+        {
+            if (i < j)
+            {
+                int q = Partition(a, i, j);
+                a = QuickSort(a, i, q);
+                a = QuickSort(a, q + 1, j);
+            }
+            return a;
+        }
+
+        private static int Partition(int[] a, int p, int r)
+        {
+            int x = a[p];
+            int i = p - 1;
+            int j = r + 1;
+            while (true)
+            {
+                do
+                {
+                    j--;
+                }
+                while (a[j] > x);
+                do
+                {
+                    i++;
+                }
+                while (a[i] < x);
+                if (i < j)
+                {
+                    int tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                }
+                else
+                {
+                    return j;
+                }
+            }
+        }
+
+
+
+        public static int[] MergeSorted(this int[] array)
+        {
+            if (array != null)
+            {
+                if (array.Length == 1)
+                {
+                    return array;
+                }
+
+                int middle = array.Length / 2;
+                return Merge(MergeSorted(array.Take(middle).ToArray()), MergeSorted(array.Skip(middle).ToArray()));
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        private static int[] Merge(int[] arr1, int[] arr2)
+        {
+            int ptr1 = 0, ptr2 = 0;
+            int[] merged = new int[arr1.Length + arr2.Length];
+
+            for (int i = 0; i < merged.Length; ++i)
+            {
+                if (ptr1 < arr1.Length && ptr2 < arr2.Length)
+                {
+                    merged[i] = arr1[ptr1] > arr2[ptr2] ? arr2[ptr2++] : arr1[ptr1++];
+                }
+                else
+                {
+                    merged[i] = ptr2 < arr2.Length ? arr2[ptr2++] : arr1[ptr1++];
+                }
+            }
+
+            return merged;
+        }
+    }
+}
