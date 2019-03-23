6c9ce26f8142b47e6156058c867645cb08a66e7d
Zlata Zueva <zlatka.zueva@gmail.com>
2019-03-23 22:07:45 +0300

    InsertNumber task implemented

---
+++ 02/NET.S.2019.Zueva.02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Zueva._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = UnusualOperationsWithNumbers.InsertNumber(15, 15, 0, 0);
+            result = UnusualOperationsWithNumbers.InsertNumber(8, 15, 0, 0);
+            result = UnusualOperationsWithNumbers.InsertNumber(8, 15, 3, 8);
+            result = UnusualOperationsWithNumbers.InsertNumber(15, 8, 1, 3);
+        }
+    }
+}
---
+++ 02/NET.S.2019.Zueva.02/UnusualOperationsWithNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Zueva._02
+{
+    public class UnusualOperationsWithNumbers
+    {
+        public static int InsertNumber (int numberSource, int numberIn, byte bitFrom, byte bitTo)
+        {
+            int bitMaskofInsertingRange = 1;
+            for (byte i=bitFrom; i<bitTo; i++)
+            {
+                bitMaskofInsertingRange <<= 1;
+                bitMaskofInsertingRange++;
+            }
+            int numberInRange = numberIn & bitMaskofInsertingRange;
+            numberInRange <<= bitFrom;
+            bitMaskofInsertingRange <<= bitFrom;
+            numberSource &= ~bitMaskofInsertingRange;
+            return numberSource + numberInRange;
+        }
+    }
+}
