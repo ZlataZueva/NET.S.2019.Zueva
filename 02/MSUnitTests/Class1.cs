9dfc59e94ac78ac0f8fea75fcb2d2a85d8f325fd
Zlata Zueva <zlatka.zueva@gmail.com>
2019-03-24 22:19:28 +0300

    added MSUnit tests for InsertNumber method

---
+++ 02/MSUnitTests/Class1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NET.S._2019.Zueva._02;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSUnitTests
+{
+    [TestClass]
+    public class InsertNumberTests
+    {
+        [TestMethod]
+        public void EqualNumbersZeroRangeInsertNumberTest()
+        {
+            //Arrange
+            int sourceNumber = 15;
+            int inNumber = sourceNumber;
+            byte i = 0;
+            byte j = i;
+            int expectedResult = sourceNumber;
+
+
+            //Act
+            int actualResult = UnusualOperationsWithNumbers.InsertNumber(sourceNumber, inNumber, i, j);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void DifferentNumbersZeroRangeInsertNumberTest()
+        {
+            //Arrange
+            int sourceNumber = 8;
+            int inNumber = 15;
+            byte i = 0;
+            byte j = i;
+            int expectedResult = 9;
+
+
+            //Act
+            int actualResult = UnusualOperationsWithNumbers.InsertNumber(sourceNumber, inNumber, i, j);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
--- 02/NET.S.2019.Zueva.02/Program.cs
+++ 02/NET.S.2019.Zueva.02/Program.cs
@@ -11,11 +11,6 @@ namespace NET.S._2019.Zueva._02
         static void Main(string[] args)
         {
 
-            double result5 = UnusualOperationsWithNumbers.FindNthRoot(1, 5, 0.0001);
-            result5 = UnusualOperationsWithNumbers.FindNthRoot(8, 3, 0.0001);
-            result5 = UnusualOperationsWithNumbers.FindNthRoot(0.001, 3, 0.0001);
-            result5 = UnusualOperationsWithNumbers.FindNthRoot(0.04100625, 4, 0.0001);
-
         }
     }
 }
