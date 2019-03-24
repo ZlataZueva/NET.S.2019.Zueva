e03482f1132d0c0b162e682a4aece4e57b3be48d
Zlata Zueva <zlatka.zueva@gmail.com>
2019-03-24 21:07:50 +0300

    InsertNumber NUnit tests added

--- 02/NET.S.2019.Zueva.02/Program.cs
+++ 02/NET.S.2019.Zueva.02/Program.cs
@@ -10,10 +10,7 @@ namespace NET.S._2019.Zueva._02
     {
         static void Main(string[] args)
         {
-            int result1 = UnusualOperationsWithNumbers.InsertNumber(15, 15, 0, 0);
-            result1 = UnusualOperationsWithNumbers.InsertNumber(8, 15, 0, 0);
-            result1 = UnusualOperationsWithNumbers.InsertNumber(8, 15, 3, 8);
-            result1 = UnusualOperationsWithNumbers.InsertNumber(15, 8, 1, 3);
+            int result1 = UnusualOperationsWithNumbers.InsertNumber(8, 15, 3, 8);
 
             int result2 = UnusualOperationsWithNumbers.FindNextBiggerNumber(12);
             result2 = UnusualOperationsWithNumbers.FindNextBiggerNumber(513);
---
+++ 02/NumbersOperationsTesting/UnusualOperationsTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using NET.S._2019.Zueva._02;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersOperationsTesting
+{
+    [TestFixture]
+    public class InsertNumberTests
+    {
+        [Test]
+        public void EqualNumbersZeroRangeInsertNumberTest ()
+        {
+            //Arrange
+            int sourceNumber = 15;
+            int inNumber = sourceNumber;
+            byte i = 0;
+            byte j = i;
+            int expectedResult = sourceNumber;
+
+
+            //Act
+            int actualResult = UnusualOperationsWithNumbers.InsertNumber(sourceNumber, inNumber, i, j);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void DifferentNumbersZeroRangeInsertNumberTest()
+        {
+            //Arrange
+            int sourceNumber = 8;
+            int inNumber = 15;
+            byte i = 0;
+            byte j = i;
+            int expectedResult = 9;
+
+
+            //Act
+            int actualResult = UnusualOperationsWithNumbers.InsertNumber(sourceNumber, inNumber, i, j);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void DifferentNumbersNonZeroRangeInsertNumberTest()
+        {
+            //Arrange
+            int sourceNumber = 15;
+            int inNumber = 8;
+            byte i = 1;
+            byte j = 3;
+            int expectedResult = 1;
+
+
+            //Act
+            int actualResult = UnusualOperationsWithNumbers.InsertNumber(sourceNumber, inNumber, i, j);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+
+
+}
